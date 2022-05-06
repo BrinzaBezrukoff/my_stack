824a58e54fe50f3e8f35841cdb0edbf0a553b827
Ilya Bezrukov <bezrukoff888@gmail.com>
2022-05-06 23:59:45 +0300

    Added simple test in main.cpp. Changed CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+#include "stack/Stack.h"
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Stack s;
+
+    s.push(10);
+    s.push(20);
+    s.pop();
+    s.push(30);
+
+    std::cout << s.top() << std::endl;
+    std::cout << s.size() << std::endl;
+
     return 0;
 }
